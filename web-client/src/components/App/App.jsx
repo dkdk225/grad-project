cb7b957ff21ef6cd89b9e7c8e0ab3f6ffbd27c7c
dkdk225 <deniz225kaya@gmail.com>
2023-11-04 08:10:41 +0300

    feat(web-client): provide an event emitter in app context

--- web-client/src/components/App/App.jsx
+++ web-client/src/components/App/App.jsx
@@ -4,14 +4,19 @@ import "./common.css"
 import { PwmControlPanel } from "../PwmControlPanel";
 import { io } from "socket.io-client";
 import { server } from "../../config";
+import {EventEmitter} from 'events'
+const eventBus = new EventEmitter()
 
 const socket = io(server.url);
+const eventBusContext = createContext();
 const socketContext = createContext();
 function App() {
   return (
     <>
       <socketContext.Provider value={socket}>
-        <PwmControlPanel deviceId="deviceId"></PwmControlPanel>
+        <eventBusContext.Provider value={eventBus}>
+          <PwmControlPanel deviceId="deviceId"></PwmControlPanel>
+        </eventBusContext.Provider>
       </socketContext.Provider>
     </>
   );
