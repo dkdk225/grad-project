6b41f8c95835f290b296cc2818dd5b96eb6a0145
dkdk225 <deniz225kaya@gmail.com>
2023-11-06 22:27:13 +0300

    feat(web-client): implement mui adapter into project

--- web-client/src/components/App/App.jsx
+++ web-client/src/components/App/App.jsx
@@ -8,17 +8,22 @@ import { PwmSchedule } from "../PwmSchedule";
 import { EventEmitter } from "events";
 const eventBus = new EventEmitter();
 const eventBusContext = createContext();
+import { LocalizationProvider } from "@mui/x-date-pickers";
+import { AdapterDayjs } from "@mui/x-date-pickers/AdapterDayjs";
+
 
 const socket = io(server.url);
 const socketContext = createContext();
 function App() {
   return (
     <>
-      <socketContext.Provider value={socket}>
-        <eventBusContext.Provider value={eventBus}>
-          <PwmSchedule></PwmSchedule>
-        </eventBusContext.Provider>
-      </socketContext.Provider>
+      <LocalizationProvider dateAdapter={AdapterDayjs}>
+        <socketContext.Provider value={socket}>
+          <eventBusContext.Provider value={eventBus}>
+            <PwmSchedule></PwmSchedule>
+          </eventBusContext.Provider>
+        </socketContext.Provider>
+      </LocalizationProvider>
     </>
   );
 }
