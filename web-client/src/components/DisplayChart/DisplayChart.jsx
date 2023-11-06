dfd80dbb79e293ba9d1f2c29b412389c6d0ca507
dkdk225 <deniz225kaya@gmail.com>
2023-11-06 22:52:15 +0300

    refactor(web-client): convert control chart into display chart

---
+++ web-client/src/components/DisplayChart/DisplayChart.jsx
@@ -0,0 +1,104 @@
+import React, { useEffect, useRef, useState } from "react";
+import Chart from "chart.js/auto";
+import "chartjs-plugin-dragdata";
+import { Line, Scatter } from "react-chartjs-2";
+import "./DisplayChart.css";
+import { secondsToHourClock } from "../../utils";
+
+//=================
+/**
+ * Transforms the given value within given interval into HH:MM format
+ * @param {number} value - Number to be transformed into HH:MM form
+ * @param {number} min - Lower limit of the interval containing value
+ * @param {number} max - Upper limit of the interval containing value
+ * @returns
+ */
+
+const mapToTime = (value, min, max) => {
+  const scale = (num, from, to) => {
+    return (
+      ((num - from.min) * (to.max - to.min)) / (from.max - from.min) + to.min
+    );
+  };
+  //there are 86,400 seconds in a day
+  //Map min-max into date
+  const newValue = scale(value, { min: min, max: max }, { min: 0, max: 86400 });
+  return secondsToHourClock(newValue);
+};
+
+const mapDateConfig = (min, max) => {
+  const config = {
+    scales: {
+      x: {
+        type: "linear",
+        min: min,
+        max: max,
+        ticks: {
+          stepSize: (max - min) / 24,
+          callback: function (value, index, ticks) {
+            return mapToTime(value, min, max);
+          },
+        },
+      },
+      y: {
+        min: 0,
+        max: 100,
+        ticks: {
+          callback: function (value, index, ticks) {
+            return "%" + value;
+          },
+        },
+      },
+    },
+    plugins: {
+      tooltip: {
+        callbacks: {
+          title: (context) => {
+            const title = context[0].label;
+            return mapToTime(Number(title), min, max);
+          },
+          label: function (context) {
+            let label = context.dataset.label || "";
+
+            if (label) {
+              label += ": ";
+            }
+            if (context.parsed.y !== null) {
+              label = "%" + context.parsed.y;
+            }
+            return label;
+          },
+        },
+      },
+    },
+  };
+  return config;
+};
+
+/**
+ * Displays a given set of data on a chart
+ * @param {Object} param0
+ * @param {}
+ * @returns
+ */
+export default function DisplayChart({ schedule }) {
+  console.log(schedule);
+  const config = mapDateConfig(0, 86400);
+
+  const points = {
+    datasets: [],
+  };
+  for (let dataset of Object.keys(schedule)) {
+    points.datasets.push({
+      tension: 0.3,
+      borderColor: schedule[dataset].chartColor,
+      data: [{ x: -1000, y: 0 }, ...schedule[dataset], { x: 87400, y: 0 }],
+    });
+  }
+
+  return (
+    <div className="display-chart">
+      <Line data={points} options={config} />
+    </div>
+  );
+}
---
+++ web-client/src/components/DisplayChart/index.js
@@ -0,0 +1,1 @@
+export { default as DisplayChart } from "./DisplayChart";
