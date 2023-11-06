c889db06e56c683fbcc1542cb42d547d0ca9ad5a
dkdk225 <deniz225kaya@gmail.com>
2023-11-06 22:45:32 +0300

    feat(web-client): create schedule class to manage pwm scheduling

---
+++ web-client/src/schedule/schedule.js
@@ -0,0 +1,83 @@
+/**
+ * Data structure storing multidimensional points in time
+ * should be treated as an immutable object
+ */
+class Schedule {
+  #fields;
+  #pointArr;
+  #essentialFields;
+  #cache;
+  /**
+   * @param {Object} options - An options object
+   * @param {Point[]} options.pointArr - Initial array of points to be stored
+   * @param {string[]} options.fields - Key values of the fields
+   */
+  constructor({ fields, pointArr = [] }) {
+    this.#fields = fields;
+    this.#pointArr = pointArr;
+    this.#cache = {};
+    // essential fields are fields related to data excluding fields such as index and time
+    this.#essentialFields = fields.filter(
+      (field) => field !== "time" || field !== "index"
+    );
+  }
+
+  /**
+   * Initiate a point
+   *
+   */
+  newPoint(values = []) {
+    const newPoint = {};
+    for (let field of this.#fields) {
+      newPoint[field] = 0;
+    }
+
+    for (let value of Object.keys(values)) {
+      newPoint[value] = values[value]
+    }
+
+    newPoint.time = -10000;
+    newPoint.index = this.#pointArr.length;
+    return new Schedule({
+      fields: this.#fields,
+      pointArr: [newPoint, ...this.#pointArr],
+    });
+  }
+
+  updatePoint(newPoint) {
+    const newPointArr = this.#pointArr.map((e) => e); // copy the point array
+    newPointArr[newPoint.index] = newPoint;
+    newPointArr.sort((e, b) => {
+      return e.time - b.time;
+    });
+    newPointArr.forEach((point, index) => {
+      point.index = index;
+    });
+    return new Schedule({ pointArr: newPointArr , fields: this.#fields});
+  }
+  /**
+   * Seperate points into displayable format at chart
+   */
+  parse() {
+    const parsed = {};
+    for (let field of this.#essentialFields) {
+      parsed[field] = [];
+    }
+    for (let point of this.#pointArr) {
+      for (let field of this.#essentialFields) {
+        parsed[field].push({ y: point[field], x:point.time});
+      }
+    }
+    return parsed;
+  }
+
+  getPointArrCopy() {
+    if (this.#cache.pointArr) {
+      return this.#cache.pointArr;
+    }
+    this.#cache.pointArr = this.#pointArr.map((e) => e); // shallow copy of pointArr
+    return this.#cache.pointArr;
+  }
+}
+
+export { Schedule };
