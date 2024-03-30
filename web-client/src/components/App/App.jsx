b89fdc64240b20494d2cc1de7264fa0aff6dba46
dkdk225 <deniz225kaya@gmail.com>
2024-03-31 01:52:18 +0300

    feat: add router

--- web-client/src/components/App/App.jsx
+++ web-client/src/components/App/App.jsx
@@ -1,7 +1,6 @@
 import { createContext, useEffect, useState } from "react";
 import "./App.css";
 import "./common.css";
-import { PwmSchedule } from "../common/PwmSchedule";
 import { EventEmitter } from "events";
 const eventBus = new EventEmitter();
 const eventBusContext = createContext(eventBus);
@@ -9,18 +8,57 @@ import { LocalizationProvider } from "@mui/x-date-pickers";
 import { AdapterDayjs } from "@mui/x-date-pickers/AdapterDayjs";
 import { RequestHandler } from "../../request-handler";
 import { LightController } from "../LightController";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import {
+  Login,
+  DeviceControl,
+  CreateAccount,
+  Navbar,
+  Redirect,
+  Devices,
+} from "../pages";
+import { paths } from "../../requests/paths";
+import { AddDeviceToUser } from "../pages/AddDeviceToUser";
+const {client, api} = paths
+
+const router = createBrowserRouter([
+  {
+    path: client.login,
+    element: <Login></Login>,
+  },
+  {
+    path: client.createAccount,
+    element: <CreateAccount></CreateAccount>,
+  },
+  {
+    path: client.addDeviceToUser,
+    element: <AddDeviceToUser></AddDeviceToUser>,
+  },
+  {
+    path: client.devices,
+    element: <Devices></Devices>,
+  },
+  {
+    path: client.deviceControl,
+    element: <DeviceControl></DeviceControl>,
+  },
+  // {
+  //   path: "*",
+  //   element: <Redirect></Redirect>,
+  // },
+]);
 
 const requestHandler = new RequestHandler();
 const requestHandlerContext = createContext(requestHandler);
 
-const deviceId = "fhgfk5";
 function App() {
   return (
     <>
       <LocalizationProvider dateAdapter={AdapterDayjs}>
         <requestHandlerContext.Provider value={requestHandler}>
           <eventBusContext.Provider value={eventBus}>
-            <LightController deviceId={deviceId}></LightController>
+            <Navbar></Navbar>
+            <RouterProvider router={router} />
           </eventBusContext.Provider>
         </requestHandlerContext.Provider>
       </LocalizationProvider>
