249c3cd027bcf1d97aaaf0cb5039db0172f36a98
dkdk225 <deniz225kaya@gmail.com>
2024-03-31 22:04:09 +0300

    feat: remember user after login in user provider

--- web-client/src/components/pages/Login/Login.jsx
+++ web-client/src/components/pages/Login/Login.jsx
@@ -2,8 +2,10 @@ import "./Login.css";
 import { Form } from "../../common/Form";
 import { getRequest, postRequest } from "../../../requests";
 import { useNavigate } from "react-router-dom";
+import { PassUserProvider } from "../../App";
 import { paths } from "../../../requests/paths";
 const { client, api } = paths;
+
 function Login() {
   const navigate = useNavigate();
   return (
@@ -16,6 +18,7 @@ function Login() {
             if (result.data) {
               localStorage.setItem("JWT", result.data);
               navigate(client.devices);
+              PassUserProvider.getUser();
             }
           });
         }}
