d1c120911f78d68869deec6b74f5327a014e6de0
dkdk225 <deniz225kaya@gmail.com>
2024-03-31 22:04:48 +0300

    feat: show user data on navbar

--- web-client/src/components/pages/Navbar/Navbar.jsx
+++ web-client/src/components/pages/Navbar/Navbar.jsx
@@ -1,14 +1,37 @@
 import "./Navbar.css";
-import { Form } from "../../common/Form";
-import { getRequest, postRequest } from "../../../requests";
 import { Button } from "@mui/material";
+import { UserContext, PassUserProvider } from "../../App";
+import { useContext } from "react";
+import { useNavigate } from "react-router-dom";
+import { paths } from "../../../requests/paths";
+import { postRequest } from "../../../requests/requests";
+const { client, api } = paths;
+
 function Navbar() {
+  const navigate = useNavigate();
+  const { user } = useContext(UserContext);
+  const userName = user ? user.userName : "";
+  const login = () => {
+    navigate(client.login);
+  };
+
+  const logout = () => {
+    postRequest({ token: localStorage.getItem("JWT") }, api.logout);
+    PassUserProvider.reset();
+    navigate(client.login);
+  };
+  const onClick = user ? logout : login;
   return (
     <nav className="navbar">
+      <span className="navbar_user-name">{userName}</span>
       <ul className="navbar_navigation-menu">
         <li>
-          <Button className="navbar_navigation-button" variant="contained">
-            Logout
+          <Button
+            className="navbar_navigation-button"
+            onClick={onClick}
+            variant="contained"
+          >
+            {user ? "Logout" : "Login"}
           </Button>
         </li>
       </ul>
