cbf8a1fe9e33855e212d29619b2757b73f6d1191
dkdk225 <deniz225kaya@gmail.com>
2024-04-30 14:07:49 +0300

    feat: add config class to store device config

---
+++ microcontroller-client/lib/config/Config.cpp
@@ -0,0 +1,3 @@
+#include "Config.h"
+
+string Config::server_DNS = "http://192.168.1.65:3000";
---
+++ microcontroller-client/lib/config/Config.h
@@ -0,0 +1,6 @@
+#include <string>
+using namespace std;
+class Config{
+  public:
+    static string server_DNS;
+};
